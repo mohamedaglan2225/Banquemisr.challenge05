6f7c85ead304843a9319d4b512bdf625c5752937
Mohamed Aglan <mohamedaglan.dev@gmail.com>
2024-07-05 21:39:27 +0300

    Initial Commit

---
+++ Banquemisr.challenge05/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banquemisr.challenge05
+//
+//  Created by Mohamed Aglan on 7/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
